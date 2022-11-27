e9735e08a09bd51444ddb0754c58631eef4c58e4
Tucker J. Pelletier <virtualdogbert@gmail.com>
2022-11-27 15:52:37 -0500

    Making the Configurator so that it will work with Spring Boot/Grails
    Also just some clean up adding features like:
    configurable file name
    optional external config file falling back to internal config
    basic error checking

--- src/main/groovy/ch/qos/logback/classic/GroovyConfigurator.groovy
+++ src/main/groovy/ch/qos/logback/classic/GroovyConfigurator.groovy
@@ -5,13 +5,40 @@ import ch.qos.logback.core.spi.ContextAwareBase
 import groovy.transform.CompileStatic
 
 @CompileStatic
-class GroovyConfigurator extends ContextAwareBase  implements Configurator{
+class GroovyConfigurator extends ContextAwareBase implements Configurator {
 
 
     @Override
     void configure(LoggerContext loggerContext) {
-        LogbackDSLInitializer.init(loggerContext, getClass().getClassLoader().getResourceAsStream("logback-config.groovy"))
+        LoggerContext context = loggerContext
+        String configName = System.getProperty('logback.config.file') ?: System.getenv('LOGBACK_CONFIG_FILE') ?: "logback-config.groovy"
+        String externalConfig = System.getProperty('logback.config.external.file') ?: System.getenv('LOGBACK_CONFIG_EXTERNAL_FILE')
+        InputStream inputStream = getClass()?.getClassLoader()?.getResourceAsStream(configName)
+
+        if (externalConfig) {
+            File file = new File(externalConfig)
+
+            try {
+                if (file && file.exists()) {
+                    inputStream = file.newInputStream()
+                    LogbackDSLInitializer.init(context, inputStream)
+                    loggerContext.putObject('org.springframework.boot.logging.LoggingSystem', new Object())
+                    return
+                }
+            }catch(Exception e){
+                println "External config for ${externalConfig} failed falling back to internal config"
+                e.printStackTrace()
+            }
+
+        }
+
+        if (context && inputStream) {
+            LogbackDSLInitializer.init(context, inputStream)
+            loggerContext.putObject('org.springframework.boot.logging.LoggingSystem', new Object())
+            return
+        }
+
+        println "logback-groovy-config dependency is installed but no groovy logback config was found in resources."
+        println "Filenames checked checked: System.getProperty('logback.config.file'), System.getenv('LOGBACK_CONFIG_FILE'), logback-config.groovy."
     }
-
-
 }
