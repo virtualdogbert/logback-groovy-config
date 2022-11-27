01955f13ae0ae76f55059092a5a1647ea0c00514
Tucker J. Pelletier <virtualdogbert@gmail.com>
2022-11-27 18:35:44 -0500

    Cleaning up and updating docs

--- src/main/groovy/ch/qos/logback/classic/GroovyConfigurator.groovy
+++ src/main/groovy/ch/qos/logback/classic/GroovyConfigurator.groovy
@@ -1,16 +1,35 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2015, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
 package ch.qos.logback.classic
 
 import ch.qos.logback.classic.spi.Configurator
 import ch.qos.logback.core.spi.ContextAwareBase
 import groovy.transform.CompileStatic
 
+/**
+ * This class gets called by Logback-classic to configure logback using the Groovy Logback DSL. This
+ */
 @CompileStatic
 class GroovyConfigurator extends ContextAwareBase implements Configurator {
 
-
+    /**
+     * This configures logback using the Groovy Logback DSL.
+     *
+     * @param loggerContext Logback context to configure.
+     */
     @Override
     void configure(LoggerContext loggerContext) {
-        LoggerContext context = loggerContext
         String configName = System.getProperty('logback.config.file') ?: System.getenv('LOGBACK_CONFIG_FILE') ?: "logback-config.groovy"
         String externalConfig = System.getProperty('logback.config.external.file') ?: System.getenv('LOGBACK_CONFIG_EXTERNAL_FILE')
         InputStream inputStream = getClass()?.getClassLoader()?.getResourceAsStream(configName)
@@ -21,7 +40,7 @@ class GroovyConfigurator extends ContextAwareBase implements Configurator {
             try {
                 if (file && file.exists()) {
                     inputStream = file.newInputStream()
-                    LogbackDSLInitializer.init(context, inputStream)
+                    LogbackDSLInitializer.init(loggerContext, inputStream)
                     loggerContext.putObject('org.springframework.boot.logging.LoggingSystem', new Object())
                     return
                 }
@@ -32,8 +51,10 @@ class GroovyConfigurator extends ContextAwareBase implements Configurator {
 
         }
 
-        if (context && inputStream) {
-            LogbackDSLInitializer.init(context, inputStream)
+        if (loggerContext && inputStream) {
+            LogbackDSLInitializer.init(loggerContext, inputStream)
+
+            //Added so that Spring Boot/Grails won't override this config with the Spring Boot defaults.
             loggerContext.putObject('org.springframework.boot.logging.LoggingSystem', new Object())
             return
         }
